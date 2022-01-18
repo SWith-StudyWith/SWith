fc3013cb6d1998a3700b87e8c2e0e690b9d337b3
한지희 <g2hhh2ee@gmail.com>
2022-01-18 21:47:41 +0900

    Feat: add components

---
+++ frontend/src/views/members/components/SignUp.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  nane: '',
+  components: {},
+  data() {
+    return {
+      sampleData: '',
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
