3f3a2bf18d79163b4a281fd8158c773156328b45
한지희 <g2hhh2ee@gmail.com>
2022-01-18 17:53:19 +0900

    Style: add font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,9 +6,11 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap';
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Mulish', 'Noto Sans KR', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
