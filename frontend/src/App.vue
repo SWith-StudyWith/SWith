bd5deb39c52d7a482eee85c88ace3f6691701448
한지희 <g2hhh2ee@gmail.com>
2022-01-18 23:09:15 +0900

    Chore: change eslint standard to airbnb

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Landing</router-link> |
+    <router-link to="/main">Main</router-link>
   </div>
   <router-view/>
 </template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 // import Home from '../views/Home.vue'
 // // main
 // import Landing from "@/views/Landing.vue";
@@ -15,69 +15,68 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import StudyMain from "@/views/studies/components/StudyMain.vue";
 // import StudyView from "@/views/studies/components/StudyView.vue";
 
-
 const routes = [
   {
     path: '/',
     name: 'Landing',
-    component: () => import(/* webpackChunkName: "landing"*/ '../views/Landing.vue')
+    component: () => import(/* webpackChunkName: "landing" */ '../views/Landing.vue'),
   },
   {
     path: '/main',
     name: 'Main',
-    component: () => import(/* webpackChunkName: "main", webpackPrefetch:true  */ '../views/main/components/Main.vue')
+    component: () => import(/* webpackChunkName: "main", webpackPrefetch:true  */ '../views/main/components/Main.vue'),
   },
   {
     path: '/members/login',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/members/components/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '../views/members/components/Login.vue'),
   },
   {
     path: '/members/signup',
     name: 'SignUp',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/members/components/SignUp.vue')
+    component: () => import(/* webpackChunkName: "signup" */ '../views/members/components/SignUp.vue'),
   },
   {
     path: '/members/mypage',
     name: 'MyPage',
-    component: () => import(/* webpackChunkName: "mypage" */ '../views/members/components/MyPage.vue')
+    component: () => import(/* webpackChunkName: "mypage" */ '../views/members/components/MyPage.vue'),
   },
   {
     path: '/members/findpassword',
     name: 'FindPassword',
-    component: () => import(/* webpackChunkName: "findpassword" */ '../views/members/components/FindPassword.vue')
+    component: () => import(/* webpackChunkName: "findpassword" */ '../views/members/components/FindPassword.vue'),
   },
   {
     path: '/members/changepassword',
     name: 'ChangePassword',
-    component: () => import(/* webpackChunkName: "changepassword" */ '../views/members/components/ChangePassword.vue')
+    component: () => import(/* webpackChunkName: "changepassword" */ '../views/members/components/ChangePassword.vue'),
   },
   {
     path: '/studies/create',
     name: 'StudyCreate',
-    component: () => import(/* webpackChunkName: "about" */ '../views/studies/components/StudyCreate.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/studies/components/StudyCreate.vue'),
   },
   {
     path: '/studies/update',
     name: 'StudyUpdate',
-    component: () => import(/* webpackChunkName: "about" */ '../views/studies/components/StudyUpdate.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/studies/components/StudyUpdate.vue'),
   },
   {
     path: '/studies/:member_id',
     name: 'StudyMain',
-    component: () => import(/* webpackChunkName: "studymain", webpackPrefetch:true  */ '../views/studies/components/StudyMain.vue')
+    component: () => import(/* webpackChunkName: "studymain", webpackPrefetch:true  */ '../views/studies/components/StudyMain.vue'),
   },
   {
     path: '/studies/:member_id/room',
     name: 'StudyView',
-    component: () => import(/* webpackChunkName: "studyview", webpackPrefetch:true  */ '../views/studies/components/StudyView.vue')
+    component: () => import(/* webpackChunkName: "studyview", webpackPrefetch:true  */ '../views/studies/components/StudyView.vue'),
   },
 
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
