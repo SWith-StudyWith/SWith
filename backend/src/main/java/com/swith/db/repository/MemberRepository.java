06d7504485ce3830910e47f20b5b983f089c0ed0
김도현 <kdh494849@gmail.com>
2022-01-19 11:18:51 +0900

    Feat: repository 추가

--- backend/src/main/java/com/swith/db/repository/MemberRepository.java
+++ backend/src/main/java/com/swith/db/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.swith.db.repository;
 
-public interface MemberRepository {
+import com.swith.db.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+    Member findByKakaoId(String kakaoId);
+    Member findByGoogleId(String googleId);
+    Member findByNickname(String nickname);
 }
