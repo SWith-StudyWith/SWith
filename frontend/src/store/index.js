832829b5faecda6967955530555a8770e764996a
이성재 <xpsxm468@naver.com>
2022-01-22 00:16:14 +0900

    Fix: vuex store index 설정

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,17 @@
+import { createStore } from 'vuex';
+
 // Vuex 조립
 import state from './state';
 import * as getters from './getters';
 import * as mutations from './mutations';
 import * as actions from './actions';
 
-const root = {
+const store = createStore({
   namespaced: true,
   state,
   getters,
   mutations,
   actions,
-};
+});
 
-export default root;
+export default store;
