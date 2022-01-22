679b276f8ad8cd35a525d1ce531ca18ea2acc5f1
이성재 <xpsxm468@naver.com>
2022-01-22 17:55:14 +0900

    Chore: import bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import "bootstrap";
 import '@fortawesome/fontawesome-free/js/all';
 
 createApp(App).use(store).use(router).mount('#app');
