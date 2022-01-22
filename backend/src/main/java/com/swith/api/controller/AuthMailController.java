58232e81d0bd062fdbf05454a390dc989ee84111
김수연 <lovesu5632@naver.com>
2022-01-22 22:02:02 +0900

    Feat: 이미 가입된 이메일 validation

--- backend/src/main/java/com/swith/api/controller/AuthMailController.java
+++ backend/src/main/java/com/swith/api/controller/AuthMailController.java
@@ -2,6 +2,7 @@ package com.swith.api.controller;
 
 import com.swith.api.request.AuthMailReq;
 import com.swith.api.service.AuthMailService;
+import com.swith.api.service.MemberService;
 import com.swith.common.response.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,15 @@ public class AuthMailController {
     @Autowired
     AuthMailService authMailService;
 
+    @Autowired
+    MemberService memberService;
+
     @PostMapping("/email")
     public ResponseEntity<BaseResponse> sendAuthMail(@RequestBody AuthMailReq authMailReq) {
+        if (memberService.getMemberByEmail(authMailReq.getEmail()) != null) {
+            return ResponseEntity.status(200).body(new BaseResponse(false, 400, "이미 가입된 이메일"));
+        }
+
         authMailService.sendAuthMail(authMailReq);
         return ResponseEntity.status(200).body(new BaseResponse(true, 200, "이메일 인증번호 전송 성공"));
     }
