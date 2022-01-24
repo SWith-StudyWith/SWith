eb9f99f44cb0265dd9f36829c5c4ad10dd5026bc
이성재 <xpsxm468@naver.com>
2022-01-24 22:54:59 +0900

    Fix: kakao client id, redirect uri .env로 이동

--- frontend/src/views/members/components/KakaoLoginBtn.vue
+++ frontend/src/views/members/components/KakaoLoginBtn.vue
@@ -1,17 +1,15 @@
 <template>
-  <a :href="KAKAO_AUTH_CODE_URL">
+  <a :href="kakaoGetAuthCodeURL">
     <img src="@/assets/img/kakao_login_medium_narrow.png" alt="kakao login button">
   </a>
 </template>
 <script>
-import { KAKAO_GET_AUTH_CODE_URL } from '@/api/kauth';
-
 export default {
   name: 'KakaoLoginBtn',
   components: {},
   setup() {
-    const KAKAO_AUTH_CODE_URL = KAKAO_GET_AUTH_CODE_URL;
-    return { KAKAO_AUTH_CODE_URL };
+    const kakaoGetAuthCodeURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.VUE_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.VUE_APP_KAKAO_REDIRECT_URI}&response_type=code`
+    return { kakaoGetAuthCodeURL };
   },
 }
 </script>
