5a4bfce44be0dabc7c667049fe0ec38a38d2a63d
이성재 <xpsxm468@naver.com>
2022-01-25 15:19:55 +0900

    Feat: 스터디 메인 페이지 컴포넌트 추가

---
+++ frontend/src/views/studies/components/StudyMain.vue
@@ -0,0 +1,29 @@
+<template>
+  <Navbar/>
+  <div>
+
+  </div>
+</template>
+<script>
+import Navbar from '@/views/common/Navbar.vue';
+
+export default {
+  name: '',
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+<style scoped>
+
+</style>
