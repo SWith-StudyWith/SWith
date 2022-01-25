7efdad60a3a514c29f06031d1b4b2ddc89828d24
김윤하 <beupthere@gmail.com>
2022-01-25 21:17:17 +0900

    Chore: vuex-persistedstate 설정

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 // 모듈 import
 import user from './modules/user';
@@ -7,6 +8,7 @@ const store = createStore({
   modules: {
     user,
   },
+  plugins: [createPersistedState()]
 });
 
 export default store;
