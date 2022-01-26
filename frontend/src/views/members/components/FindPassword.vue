35d57a0c7bb6059e40c3abfa1ae144bc1ff84954
박주미 <zummy@kakao.com>
2022-01-26 23:48:30 +0900

    Feat: 이메일 유효성 검사 추가

--- frontend/src/views/members/components/FindPassword.vue
+++ frontend/src/views/members/components/FindPassword.vue
@@ -7,10 +7,12 @@
         class="findpassword-form"
       >
         <label for="email" class="form-label">이메일</label>
-        <input class="form-control" id="email" type="email"
+        <input class="form-control" id="email" type="email" v-model="state.email"
           placeholder="가입하신 이메일을 입력해주세요"
-          style="background-color: #F4F5F4; vertical-align: middle;"
+          style="background-color: #F4F5F4; vertical-align: middle;" required
         >
+        <div :style="{ visibility: (state.isValidEmail || !state.wasInputed.email)? 'hidden' : 'visible' }"
+          class="invalid-feedback">유효하지 않은 이메일입니다.</div>
       </form>
     </div>
     <br>
@@ -26,15 +28,50 @@
 </template>
 
 <script>
+/* eslint-disable */
 import Navbar from '@/views/common/Navbar.vue';
 import Footer from '@/views/common/Footer.vue';
+
+import { reactive, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
 export default {
   name: 'FindPassword',
   components: {
     Navbar,
     Footer
-  }
+  },
+  setup(){
+    const state = reactive({
+      email: '',
+
+      wasInputed: {
+        email: false,
+      },
+
+      isValidEmail: computed(() => {
+        if (state.email !== '') {
+          state.wasInputed.email = true;
+        }
+        if (state.email && validateEmail(state.email)) {
+          return true;
+        }
+        return false;
+      }),
+    })
+
+    const validateEmail = function (email) {
+      const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+      return re.test(email);
+    };
+
+    return{
+      state,
+    }
+
+  },
+
 };
 </script>
 
@@ -70,8 +107,16 @@ margin-top: 20px;
 input[type="email"] {
   height: 42px;
   padding: 10px 15px;
+  margin-bottom: 3px;
 }
 button{
   text-align: center;
+}
+
+.invalid-feedback {
+  display: block;
+  font-size: 0.75rem;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
 }
 </style>
