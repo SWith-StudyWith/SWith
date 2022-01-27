af6d3bffda6a0309d25fca557e420855725caea8
한지희 <g2hhh2ee@gmail.com>
2022-01-27 14:23:56 +0900

    Style: 기본 스터디 페이지 레이아웃 조정

--- frontend/src/views/studies/Studies.vue
+++ frontend/src/views/studies/Studies.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="study">
     <Sidebar />
-    <h1>This is a study page</h1>
     <div :style="{ 'margin-left': sidebarWidth }">
+    <h1>스터디 기본 페이지</h1>
     <router-view />
+    <h1>여기에 컴포넌트 넣으면 됨</h1>
     </div>
   </div>
 </template>
