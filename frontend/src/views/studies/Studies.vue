949f999ac35334a5ff7b8e5b79f98cc228480f81
한지희 <g2hhh2ee@gmail.com>
2022-01-27 14:29:08 +0900

    Style: study component items 레이아웃 조정

--- frontend/src/views/studies/Studies.vue
+++ frontend/src/views/studies/Studies.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="study">
+  <div class="study-base-page">
     <Sidebar />
     <div :style="{ 'margin-left': sidebarWidth }">
     <h1>스터디 기본 페이지</h1>
     <router-view />
     <h1>여기에 컴포넌트 넣으면 됨</h1>
+    <h1>화면모드~~</h1>
     </div>
   </div>
 </template>
@@ -24,5 +25,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.study-base-page {
+  display: flex;
+  justify-content: center;
+  flex-wrap: nowrap;
+  flex-direction: column;
+}
 </style>
