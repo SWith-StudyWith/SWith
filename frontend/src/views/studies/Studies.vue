5733fb35e6310286fb662f68dd26172b104cbd60
한지희 <g2hhh2ee@gmail.com>
2022-01-29 21:34:15 +0900

    Style: 스터디룸 배경색 추가

--- frontend/src/views/studies/Studies.vue
+++ frontend/src/views/studies/Studies.vue
@@ -2,10 +2,8 @@
   <div class="study-base-page">
     <Sidebar @show-screenmode="showScreenMode"/>
     <div :style="{ 'margin-left': sidebarWidth }">
-    <h1>스터디 기본 페이지</h1>
     <h2>카메라 들어가는 부분</h2>
     <!-- <router-view /> -->
-    <h1>여기에 컴포넌트 넣으면 됨</h1>
     <!-- <button @click="onClickKanbanBoard">칸반보드열어말어</button>
     <button @click="onClickScreenShare">화면공유열어말어</button>
     <button @click="onClickWhiteBoard">화이트보드열어말어</button> -->
@@ -87,5 +85,9 @@ export default {
   justify-content: center;
   flex-wrap: nowrap;
   flex-direction: column;
+
+  height: 100%;
+
+  background-color: #7285A6;
 }
 </style>
