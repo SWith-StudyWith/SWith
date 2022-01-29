9ffa5ed1430a9b1350a6e437d20bfc382afdb667
한지희 <g2hhh2ee@gmail.com>
2022-01-29 16:01:44 +0900

    Feat: add kanbanboard code in study page

--- frontend/src/views/studies/Studies.vue
+++ frontend/src/views/studies/Studies.vue
@@ -3,7 +3,8 @@
     <Sidebar @show-screenmode="showScreenMode"/>
     <div :style="{ 'margin-left': sidebarWidth }">
     <h1>스터디 기본 페이지</h1>
-    <router-view />
+    <h2>카메라 들어가는 부분</h2>
+    <!-- <router-view /> -->
     <h1>여기에 컴포넌트 넣으면 됨</h1>
     <!-- <button @click="onClickKanbanBoard">칸반보드열어말어</button>
     <button @click="onClickScreenShare">화면공유열어말어</button>
--- frontend/src/views/studies/components/screen/KanbanBoard.vue
+++ frontend/src/views/studies/components/screen/KanbanBoard.vue
@@ -6,20 +6,46 @@
   <h1>The sidebar is {{ collapsed ? 'closed' : 'open' }}</h1>
   <button @click="toggleSidebar">Toggle Sidebar</button>
     </div> -->
-  <h1>칸반보드컴포넌트</h1>
+    <h1>칸반보드컴포넌트</h1>
+    <div>
+      <div class="py-12 d-flex justify-content-center">
+        <div
+          v-for="tasks in kanbanBoard"
+          :key="tasks.taskId"
+          class="p-1 me-4 w-100"
+        >
+          <p class="">{{ tasks.taskName }}</p>
+          <KanbanBoardCard
+            v-for="(task) in tasks.kanban"
+            :key="task.kanbanId"
+            :task="task"
+            class="mt-3 align-items-center"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { collapsed, toggleSidebar, sidebarWidth } from '@/views/studies/components/sidebar/state.js';
 // import Sidebar from '@/views/studies/components/sidebar/Sidebar.vue';
+import { useStore } from 'vuex';
+import KanbanBoardCard from '@/views/studies/components/screen/KanbanBoardCard.vue';
+
 
 export default {
+  name: 'KanbanBoard',
   components: {
     // Sidebar,
+    KanbanBoardCard,
   },
   setup() {
-    return { collapsed, toggleSidebar, sidebarWidth }
+    const store = useStore();
+    const kanbanBoard = store.state.study.studyInfo.kanbanBoard;
+    return { collapsed, toggleSidebar, sidebarWidth,
+            kanbanBoard,
+    }
   }
 }
 </script>
