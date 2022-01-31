977a96b9bf5b804f6b2164a25362c38b52de49c9
김도현 <kdh494849@gmail.com>
2022-01-31 10:56:18 +0900

    Refactor: file upload 로직 변경, file size 제한 추가

--- backend/src/test/java/com/swith/SwithApplicationTests.java
+++ backend/src/test/java/com/swith/SwithApplicationTests.java
@@ -27,29 +27,29 @@ class SwithApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	void firebaseTest() throws IOException, CloneNotSupportedException {
-		log.debug("FirebaseConfig - {}", firebaseConfig);
-
-		FirebaseConfig config = new FirebaseConfig();
-		config.setType(firebaseConfig.getType());
-		config.setProject_id(firebaseConfig.getProject_id());
-		config.setPrivate_key_id(firebaseConfig.getPrivate_key_id());
-		config.setPrivate_key(firebaseConfig.getPrivate_key());
-		config.setClient_email(firebaseConfig.getClient_email());
-		config.setClient_id(firebaseConfig.getClient_id());
-		config.setAuth_uri(firebaseConfig.getAuth_uri());
-		config.setToken_uri(firebaseConfig.getToken_uri());
-		config.setAuth_provider_x509_cert_url(firebaseConfig.getAuth_provider_x509_cert_url());
-		config.setClient_x509_cert_url(firebaseConfig.getClient_x509_cert_url());
-		ObjectMapper mapper = new ObjectMapper();
-		String jsonString = mapper.writeValueAsString(config);
-		InputStream inputStream = IOUtils.toInputStream(jsonString, StandardCharsets.UTF_8);
-		this.storageOptions = StorageOptions.newBuilder()
-				.setProjectId(firebaseConfig.getProject_id())
-				.setCredentials(GoogleCredentials.fromStream(inputStream)).build();
-
-		log.debug("storageOptions: {}", storageOptions);
-	}
+//	@Test
+//	void firebaseTest() throws IOException, CloneNotSupportedException {
+//		log.debug("FirebaseConfig - {}", firebaseConfig);
+//
+//		FirebaseConfig config = new FirebaseConfig();
+//		config.setType(firebaseConfig.getType());
+//		config.setProject_id(firebaseConfig.getProject_id());
+//		config.setPrivate_key_id(firebaseConfig.getPrivate_key_id());
+//		config.setPrivate_key(firebaseConfig.getPrivate_key());
+//		config.setClient_email(firebaseConfig.getClient_email());
+//		config.setClient_id(firebaseConfig.getClient_id());
+//		config.setAuth_uri(firebaseConfig.getAuth_uri());
+//		config.setToken_uri(firebaseConfig.getToken_uri());
+//		config.setAuth_provider_x509_cert_url(firebaseConfig.getAuth_provider_x509_cert_url());
+//		config.setClient_x509_cert_url(firebaseConfig.getClient_x509_cert_url());
+//		ObjectMapper mapper = new ObjectMapper();
+//		String jsonString = mapper.writeValueAsString(config);
+//		InputStream inputStream = IOUtils.toInputStream(jsonString, StandardCharsets.UTF_8);
+//		this.storageOptions = StorageOptions.newBuilder()
+//				.setProjectId(firebaseConfig.getProject_id())
+//				.setCredentials(GoogleCredentials.fromStream(inputStream)).build();
+//
+//		log.debug("storageOptions: {}", storageOptions);
+//	}
 
 }
