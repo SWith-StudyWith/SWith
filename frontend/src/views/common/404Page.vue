e362cd3ef6b89ec7451f607a93c147905c9e0eb7
박주미 <zummy@kakao.com>
2022-02-01 20:25:22 +0900

    Feat: 404 error page 레이아웃 추가

--- frontend/src/views/common/404Page.vue
+++ frontend/src/views/common/404Page.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="not-found">
-    404 Not Found
+    <!-- 404 Not Found -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <img class="d-inline-block" src="@/assets/img/error_page/404.png">
+        </div>
+        <div class="col-12 ">
+          <!-- <router-link :to="{ name: 'Main' }"> -->
+            <button type="button" class="btn" style="color:white;">Go Back Main</button>
+          <!-- </router-link> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,8 +21,18 @@ export default {
 }
 </script>
 <style scoped>
+
 .not-found {
   font-size: 10rem;
   font-weight: bold;
+  height: 100vh;
+  background: #F1F6FF;
+}
+img{
+  margin-top: 250px;
+}
+button{
+  background-color: #3F4966;
+  box-shadow: 0px 4px blur #000000;
 }
 </style>
