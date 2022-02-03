ebb0f2c0e5c88a2a25a40d6127b416f0789bc146
박주미 <zummy@kakao.com>
2022-02-03 16:38:57 +0900

    Feat: studyroom sidebar 스터디 회원 목록 view 추가

---
+++ frontend/src/views/studies/components/sidebar/SidebarMemberView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    회원 목록
+    <div v-if="members.length">
+      <div class="row" v-for="member in members" :key="member.memberId">
+        <div class="col">
+          <img :src="member.imgUrl?member.imgUrl:require(`@/assets/img/navbar/profile.png`)" :fit="fit" class="profile-img">
+        </div>
+        <div class="col">
+          {{ member.nickname }}
+        </div>
+      </div>
+    </div>
+    <div v-if="members.length === 0">
+      없는데용 ?
+    </div>
+  </div>
+</template>
+
+<script>
+// import { reactive } from '@vue/reactivity'
+
+export default {
+  props:{
+    members: Array,
+  },
+  setup() {
+
+  },
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+
+<style>
+/* .img-form-label{
+  width: 150px;
+  height: 150px;
+  border-radius: 70%;
+} */
+.profile-img {
+  width: 50px;
+  height: 50px;
+  /* object-fit: cover; */
+  border-radius: 70%;
+}
+</style>
