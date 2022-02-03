92032a241f230f757241920139805500c3816ded
김수연 <lovesu5632@naver.com>
2022-02-03 11:58:26 +0900

    Fix: STOMP CORS 설정 및 401 error 해결

--- backend/src/main/java/com/swith/config/WebSocketConfig.java
+++ backend/src/main/java/com/swith/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
