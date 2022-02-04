6b2dd34b64af9dc49c5ac8d443aae7c47949d7ea
이성재 <xpsxm468@naver.com>
2022-02-04 15:47:05 +0900

    Fix: ScreenShare -> MainScreen으로 이름 변경

---
+++ frontend/src/views/studies/components/screen/MainScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="screenshare">
+    <h1>메인스크린 컴포넌트</h1>
+    <div id="main-video">
+      <user-video
+        :stream-manager="streamManager"
+        :mainStream="true"
+        v-on:dblclick="deleteMainVideoStreamManager"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserVideo from "@/views/studies/components/room/video//UserVideo.vue";
+
+export default {
+  components: {
+    UserVideo,
+  },
+  props: {
+    streamManager: Object,
+  },
+  setup() {
+  }
+}
+</script>
