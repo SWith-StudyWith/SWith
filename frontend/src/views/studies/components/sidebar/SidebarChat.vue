409599527e47f14d14104c9146c5adcb6f26c05f
박주미 <zummy@kakao.com>
2022-02-04 15:12:11 +0900

    Style: studyroom sidebar 채팅 레이아웃 적용

--- frontend/src/views/studies/components/sidebar/SidebarChat.vue
+++ frontend/src/views/studies/components/sidebar/SidebarChat.vue
@@ -8,15 +8,26 @@
         :key="idx"
         class="chat"
       >
-        <span>
-          <img :src="item.imgUrl?item.imgUrl:require(`@/assets/img/navbar/profile.png`)" alt="" aria-expanded="false">
-          <p class="nickname">{{ item.nickname }}</p>
-        </span>
-        <div class="content">내용: {{ item.content }}</div>
-        <div class="content">{{ item.createdAt }}</div>
+        <!-- 상대가 보낸 메세지  -->
+        <div class="chat-other-message">
+          <div class="chat-other-img">
+            <img :src="item.imgUrl?item.imgUrl:require(`@/assets/img/navbar/profile.png`)" alt="" aria-expanded="false">
+          </div>
+          <div class="chat-other-content">
+            <div class="chat-other-content1">
+              <p class="chat-other-nickname">{{ item.nickname }}</p>
+              <p class="chat-other-message-time">{{ item.createdAt }}</p>
+            </div>
+            <!-- <p class="chat-other-nickname">{{ item.memberId }}</p> -->
+            <div class="chat-other-content2">
+              <p class="other-content">{{ item.content }}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div>
+    <hr>
+    <div class="chat-input" id="chat-input">
       <div class="inputText">
         <input
           v-model="message"
@@ -69,6 +80,7 @@ export default {
           nickname: this.getUserInfo.nickname,
           content: this.message
         };
+        // alert(msg.memberId);
         this.stompClient.send("/receive", JSON.stringify(msg), {});
       }
     },
@@ -124,40 +136,36 @@ export default {
   margin-bottom: 30px;
 }
 img {
-  width: 30px;
-  height: 30px;
+  width: 40px;
+  height: 40px;
   border-radius: 70%;
 }
-.nickname{
-  font-size: 17px;
-  margin-bottom: 2px;
+.chat-input{
+  display: flex;
 }
-.email{
-  font-size: 13px;
-  color: rgb(199, 199, 199);
-}
-
 .inputText{
-  position: fixed;
   bottom: 0;
   margin: 10px;
-  /* display: block; */
 
   display: flex;
+  width: 100%;
 }
 input{
   /* width: 100%; */
-  height: 100px;
+  height: 90px;
   border: none;
-  border-radius: 3%;
+  border-radius: 10px;
 
   flex-grow: 1;
+
+  /* input 클릭 시, 테두리 없애기 */
+  outline: none;
+  margin-bottom: 20px;
 }
 
 .chat-userinfo-box{
   margin: 10px;
 }
-
 
 
 /*  */
@@ -170,12 +178,56 @@ input{
   scroll-behavior: smooth;
 }
 .chat {
-  background: #BDBDBD;
   border-radius: 10px;
-  padding: 1rem;
-  width: fit-content;
+  /* padding: 1rem; */
+  padding-bottom: 15px;
+  /* width: fit-content; */
 }
 .chat-body::-webkit-scrollbar {
   display: none;
+}
+.chat-other-message{
+  display: flex;
+}
+.chat-other-img{
+  margin-right: 1rem;
+}
+.chat-other-nickname{
+  font-size: 14px;
+  font-weight: 700;
+  margin-top: 0;
+  margin-block-end: 0rem;
+}
+.chat-other-content{
+  width: 100%;
+}
+/* nickname + date */
+.chat-other-content1{
+  display: flex;
+  align-items: flex-end;
+  line-break: anywhere;
+  justify-content: flex-end;
+  width: 100%;
+}
+/* message content */
+.chat-other-content2{
+  display: flex;
+  align-items: flex-end;
+  line-break: anywhere;
+}
+.other-content{
+  margin: 0.4rem 1rem 0 0;
+  border-radius: 0px 10px 10px 10px;
+  background-color: #f3f3f3;
+  max-width: 180px;
+  color: #414141;
+  padding: 0.8rem;
+  font-size: 14px;
+}
+.chat-other-message-time {
+  margin: 0;
+  font-size: 10px;
+  color: #9c9c9c;
+  margin-left: auto;
 }
 </style>
