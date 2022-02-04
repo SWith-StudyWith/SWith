678c74bb795480c4dd2dc1682da377c83b869293
김수연 <lovesu5632@naver.com>
2022-02-04 13:36:25 +0900

    Feat: 파일 1개 Drag & Drop 업로드 구현

--- frontend/src/views/studies/components/sidebar/SidebarFile.vue
+++ frontend/src/views/studies/components/sidebar/SidebarFile.vue
@@ -1,13 +1,50 @@
 <template>
-  <span>파일 업로sdfsdfdf드</span>
+  <div>
+    <h1>DropZone</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile" />
+    <span class="file-info">File: {{dropzoneFile.name}}</span>
+  </div>
 </template>
 
 <script>
+import DropZone from '@/views/studies/components/sidebar/SidebarFileDropzone.vue';
+import { ref } from "vue";
+
 export default {
-  name: 'SidebarFile',
+  name: "SidebarFile",
+  components: {
+    DropZone,
+  },
+  setup() {
+    let dropzoneFile = ref("");
+
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0]
+    };
+
+    const selectedFile = () => {
+      dropzoneFile.value = document.querySelector('.dropzoneFile').files[0];
+    }
+
+    return { dropzoneFile, drop, selectedFile };
+  },
 }
 </script>
 
 <style scoped>
-
+.home {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f1f1;
+}
+.home h1 {
+  font-size: 40px;
+  margin-bottom: 32px;
+}
+.home .file-info {
+  margin-top: 32px;
+}
 </style>
---
+++ frontend/src/views/studies/components/sidebar/SidebarFileDropzone.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{ 'active-dropzone': active }"
+    class="dropzone"
+  >
+    <span>Drag or Drop File</span>
+    <span>OR</span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile" class="dropzoneFile" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "SidebarFileDropzone",
+  setup() {
+    const active = ref(false);
+
+    const toggleActive = () => {
+      active.value = !active.value;
+    };
+
+    return { active, toggleActive }
+  },
+};
+</script>
+
+<style scoped>
+.dropzone {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px dashed #41b883;
+  background-color: #fff;
+  transition: 0.3s ease all;
+}
+.dropzone label {
+  padding: 8px 12px;
+  color: #fff;
+  background-color: #41b883;
+  transition: 0.3s ease all;
+}
+.dropzone input {
+  display: none;
+}
+
+.active-dropzone {
+  color: #fff;
+  border-color: #fff;
+  background-color: #41b883;
+}
+.active-dropzone label {
+  background-color: #fff;
+  color: #41b883;
+}
+</style>
