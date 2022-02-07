bbd9b1868755fc317baa09072045e2a2f4151887
김도현 <kdh494849@gmail.com>
2022-02-07 20:50:47 +0900

    Fix: 파일 이름 및 파일 데이터 저장

--- backend/src/main/java/com/swith/db/repository/FileRepository.java
+++ backend/src/main/java/com/swith/db/repository/FileRepository.java
@@ -1,11 +1,7 @@
 package com.swith.db.repository;
 
 import com.swith.db.entity.File;
-import com.swith.db.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FileRepository extends JpaRepository<File, Long> {
-    List<File> findByStudy(Study study);
 }
