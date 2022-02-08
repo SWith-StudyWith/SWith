dae49c843098099d870eeea65a02abfc8020a28d
김도현 <kdh494849@gmail.com>
2022-02-08 11:26:02 +0900

    Feat: 파일 다운로드 API

--- backend/src/main/java/com/swith/db/repository/FileRepository.java
+++ backend/src/main/java/com/swith/db/repository/FileRepository.java
@@ -5,7 +5,9 @@ import com.swith.db.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FileRepository extends JpaRepository<File, Long> {
     List<File> findByStudy(Study study);
+    Optional<File> findByStudyAndFileId(Study study, long fileId);
 }
