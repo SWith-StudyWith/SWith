3b0cdbbff6f92c32865b1dc2183700b4f74f075b
한지희 <g2hhh2ee@gmail.com>
2022-02-08 18:14:18 +0900

    Feat: 사이드바 파일 리스트 컴포넌트 추가

---
+++ frontend/src/views/studies/components/sidebar/SidebarFileList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="file-list-body">
+    <h1>SidebarFileList.vue</h1>
+    <p>-------------------------------------------------------</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarFileList",
+  setup() {
+
+  }
+}
+</script>
+
+<style scoped>
+.file-list-body {
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  opacity: 0.7;
+}
+</style>
