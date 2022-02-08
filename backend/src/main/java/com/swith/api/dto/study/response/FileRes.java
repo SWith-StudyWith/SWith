7a042102bd7451bd30c405bb9fcb5a999600baa5
한지희 <g2hhh2ee@gmail.com>
2022-02-08 22:56:10 +0900

    Fix: 파일 목록 조회 api 날짜 포맷 추가

--- backend/src/main/java/com/swith/api/dto/study/response/FileRes.java
+++ backend/src/main/java/com/swith/api/dto/study/response/FileRes.java
@@ -1,5 +1,6 @@
 package com.swith.api.dto.study.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,5 +17,6 @@ public class FileRes {
     private String originName;
     private String fileUrl;
     private Long fileSize;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime createdAt;
 }
